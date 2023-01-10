5320a19dab6af5d1230b602b6633f2e225ace945
Jose Tone <developer.tony17@gmail.com>
2023-01-10 10:48:24 +0100

    Initial Commit

---
+++ button-loading-animation/button_loading_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  button_loading_animationApp.swift
+//  button-loading-animation
+//
+//  Created by José Tony on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct button_loading_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
